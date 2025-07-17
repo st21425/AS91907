0cdf8dddbec17c23122bb17f3c9230c31f8bd211
st21425 <st21425@howick.school.nz>
2025-07-18 10:02:37 +1200

    Hand check finished
    It now works with any input value

--- Round/hand_check.py
+++ Round/hand_check.py
@@ -1,10 +1,11 @@
 import json
 
-played = [1,1]
+played = [7,7,1,1,1,7]# Example played dice
+
+#makes list of scoring and unscoring dies
 scoring = []
 unscoring = []
-
-for i in played:
+for i in played: 
     if i in scoring:
         scoring.append(i)
     elif i in unscoring:
@@ -16,8 +17,24 @@ for i in played:
 if len(scoring) == 0:
     scoring = [0]
 
+#counts how many of each die is played
+numbers = {}
+for i in scoring:
+    if i in numbers:
+        numbers[i] += 1
+    else:
+        numbers[i] = 1
+
+#format it in a way that can be compared to the json
+played_type = []
+for key, value in numbers.items():
+    played_type.append(value)
+
+#loads json
 with open("hands.json", "r") as file: 
     hands = json.load(file)
-for hand in hands:
-    if sorted(scoring) == hands[hand]["dies"]:
+
+#checks what hand is played
+for hand in hands: 
+    if sorted(played_type) == hands[hand]["dies"]:
         print(hand)
