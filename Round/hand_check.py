68a63fdaa11c6cdb01eb3c26e809fb96f4ef6621
st21425 <st21425@howick.school.nz>
2025-07-18 10:21:20 +1200

    Fixed it for single hand cards
    I didn't realize it was broken and is now fixed

--- Round/hand_check.py
+++ Round/hand_check.py
@@ -2,6 +2,8 @@ import json
 
 played = [7,7,1,1,1,7]# Example played dice
 
+played = [66,4,1]# Example played dice
+
 #makes list of scoring and unscoring dies
 scoring = []
 unscoring = []
@@ -15,15 +17,20 @@ for i in played:
     else:
         unscoring.append(i)
 if len(scoring) == 0:
-    scoring = [0]
+    unscoring.sort()
+    scoring = unscoring[-1]
 
 #counts how many of each die is played
 numbers = {}
-for i in scoring:
-    if i in numbers:
-        numbers[i] += 1
-    else:
-        numbers[i] = 1
+try:
+    if len(scoring) > 1:
+        for i in scoring:
+            if i in numbers:
+                numbers[i] += 1
+            else:
+                numbers[i] = 1
+except TypeError:
+    numbers[scoring] = 1
 
 #format it in a way that can be compared to the json
 played_type = []
@@ -37,4 +44,4 @@ with open("hands.json", "r") as file:
 #checks what hand is played
 for hand in hands: 
     if sorted(played_type) == hands[hand]["dies"]:
-        print(hand)
+        played_hand = hand
