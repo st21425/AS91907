192f71b807c2b75a56b8127ca5ddbdef66b8a1e7
st21425 <st21425@howick.school.nz>
2025-07-18 10:44:57 +1200

    Now includes the locking

--- Round/roll.py
+++ Round/roll.py
@@ -1,14 +1,27 @@
 import json
 import random
 
+dies_locked = {"die1": False, "die2": False, "die3": False, "die4": False, "die5": False, "die6": False}
+
 with open("dice.json", "r") as file: 
     dies = json.load(file)
 
+dice_score = {}
+
+
+for i in range(len(dies_locked)):
+    if dies_locked[f"die{i+1}"] == True:
+        continue # Skip locked dice
+    elif dies_locked[f"die{i+1}"] == False:
+        num_sides = len(dies[f"die{i+1}"])
+        die = random.randint(1, num_sides)
+        value = dies[f"die{i+1}"][f"side{die}"]["value"]
+        dice_score[f"die{i+1}"] = value
+    else:
+        print("Error: Invalid die state")
+
 played = []
 
-for i in range(6):
-    num_sides = len(dies[f"die{i+1}"])
-    die = random.randint(1, num_sides)
-    value = dies[f"die{i+1}"][f"side{die}"]["value"]
+for key, value in dice_score.items():
     played.append(value)
-
+print(played)
