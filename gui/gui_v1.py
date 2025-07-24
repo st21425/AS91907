92a96a8f4eed2555a64a5f10618e3ec4721f130e
st21425 <st21425@howick.school.nz>
2025-07-25 10:49:57 +1200

    its broken

--- gui/gui_v1.py
+++ gui/gui_v1.py
@@ -17,11 +17,15 @@ class GUI():
 
         self.button_colour = "#B6AADD"
         self.bg_colour = "#746aff"
+        self.sidebar_colour = "#46444D"
+        self.sidebar_box_colour = "#5A5A6D"
+        self.sidebar_text = "#FFFFFF"
 
         self.frames = {}
         self.frames["MainMenu"] = self.menu()
         self.frames["ShopMenu"] = self.shop()
         self.frames["GameMenu"] = self.game()
+        self.frames["Sidebar"] = self.sidebar()
 
         self.show_frames("MainMenu")
     
@@ -43,7 +47,7 @@ class GUI():
         self.game_button = Button(frame, text="New Game", bg=self.button_colour, font="Arial 12", command=lambda: self.show_frames("GameMenu"))
         self.game_button.grid(padx=350, pady=10, sticky=NSEW)
 
-        self.quit_button = Button(frame, text="Quit", bg=self.button_colour, font="Arial 12", command=lambda: self.show_frames("GameMenu"))
+        self.quit_button = Button(frame, text="Quit", bg=self.button_colour, font="Arial 12", command=self.quit)
         self.quit_button.grid(padx=350, pady=10, sticky=NSEW)
 
         return frame
@@ -54,10 +58,40 @@ class GUI():
     
     def game(self):
         frame = Frame(self.container)
+        
+        sidebar = self.sidebar()
+        sidebar.pack(side="left", fill="y")
+
+        
+
+
+        return frame
+    
+    def sidebar(self):
+        frame = Frame(self.container)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.configure(bg=self.bg_colour)
+
+        frame.rowconfigure(list(range(12)), weight = 1)
+        frame.columnconfigure(0, weight = 1)
+
+        ####### PLACEHOLDERS #######
+        self.requirement = 300 
+        self.score = 1000
+        self.mult = 1
+        self.total = 1000
+
+        self.requirement_label = Label(frame, font = "Arial 12", text = f"Requirement: {self.requirement}", bg=self.bg_colour)
+        self.requirement_label.grid(padx=1, pady=10, sticky=EW)
+
+        self.scoring_label = Label(frame, font = "Arial 12", text = f"{self.score} X {self.mult}", bg=self.sidebar_box_colour, fg=self.sidebar_text)
+        self.scoring_label.grid(padx=1, pady=10, sticky=EW)
         return frame
     
     def run(self):
         self.root.mainloop()
-    
-game  = GUI()
-game.run()
+    def quit(self):
+        self.root.destroy()
+
+GAME_RUNNING = GUI()
+GAME_RUNNING.run()
